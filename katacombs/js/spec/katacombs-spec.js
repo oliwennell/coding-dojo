840dad554a400770fcf41fb6e23b4937702a8805
Oli Wennell <oli.wennell@gmail.com>
2019-04-12 10:05:54 +0100

    :sparkles: Adding starter for JS

---
+++ katacombs/js/spec/katacombs-spec.js
@@ -0,0 +1,7 @@
+describe("katacombs", () => {
+    
+    it("Returns meaning of life", () => {
+        
+        expect(new Game().meaningOfLife()).toBe(42);
+    })
+});
---
+++ katacombs/js/src/katacombs.js
@@ -0,0 +1,7 @@
+function Game() {
+}
+
+Game.prototype.meaningOfLife = () => {
+    console.log("Hello, world!");
+    return 42;
+}
