a58129317055ce605486237c22f6428e71bde16e
Oli Wennell <oli.wennell@gmail.com>
2019-04-12 14:42:59 +0100

    :white_check_mark: Output from April 12th's dojo

--- katacombs/csharp/katacombs.tests/UnitTest1.cs
+++ katacombs/csharp/katacombs.tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using katacombs;
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
@@ -11,9 +12,46 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void When_game_starts_player_is_at_origin()
         {
-            Assert.That(Class1.MeaningOfLife(), Is.EqualTo(42));
+            var player = new Player();
+            Assert.That(player.Position, 
+                Is.EquivalentTo(new[]{0,0,0}));
+        }
+
+        [Test]
+        public void When_game_starts_should_have_a_player()
+        {
+            var game = new Game();
+            Assert.That(game.Player,
+                Is.InstanceOf(typeof(Player)));
+        }
+
+        [Test]
+        public void When_game_starts_should_have_a_map()
+        {
+            var game = new Game();
+            Assert.That(game.GameMap,
+                Is.InstanceOf(typeof(GameMap)));
+        }
+
+        [TestCase(0,0,0, "title zero")]
+        [TestCase(0,0,1, "title one")]
+        public void When_getTitle_is_called_on_map_returns_title(int x, int y, int z, string expectedTitle)
+        {
+            var thisMap = new GameMap();
+            Assert.That(thisMap.GetTitle(new [] {x, y, z}), 
+                Is.EqualTo(expectedTitle));
+        }
+
+
+        [TestCase(0,0,0, "description 0")]
+        [TestCase(0,0,1, "description 1")]
+        public void When_getDescription_is_called_on_map_returns_description(int x, int y, int z, string expectedDescription)
+        {
+            var thisMap = new GameMap();
+            Assert.That(thisMap.GetDescription(new [] {x,y,z}), 
+                Is.EqualTo(expectedDescription));
         }
     }
 }
--- katacombs/csharp/katacombs/Class1.cs
+++ katacombs/csharp/katacombs/Class1.cs
@@ -1,9 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace katacombs
 {
     public static class Class1
     {
         public static int MeaningOfLife() => 42;
+    }
+
+    public class Player
+    {
+        public Player()
+        {
+            Position = new int[]{0,0,0};
+        }
+
+        public int[] Position { get; }
+    }
+
+    public class Game
+    {
+        public Player Player { get; set; }
+        public GameMap GameMap { get; set; }
+        public Game()
+        {
+            Player = new Player();
+            GameMap = new GameMap();
+        }
+    }
+
+    public class GameMap
+    {
+        public string GetTitle(int[] location)
+        {
+            return Location[$"{location[0]},{location[1]},{location[2]}"].Title;
+        }
+        public string GetDescription(int[] location)
+        {
+            return Location[$"{location[0]},{location[1]},{location[2]}"].Description;
+        }
+
+        Dictionary<string, Location> Location = new Dictionary<string,Location> {
+            {"0,0,0", new Location("title zero", "description 0")},
+            {"0,0,1", new Location("title one", "description 1")}
+        };
+    }
+
+    public class Location
+    {
+        public string Title { get; }
+        public string Description { get; }
+
+        public Location(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
     }
 }
