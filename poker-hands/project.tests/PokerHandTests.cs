091debf16d7828fd1111a024dd23b316d804d801
Alex Hutchings <alexhutchings4@gmail.com>
2019-10-04 14:58:08 +0100

    Defines all the hands with tests :tada:

--- poker-hands/project.tests/PokerHandTests.cs
+++ poker-hands/project.tests/PokerHandTests.cs
@@ -91,7 +91,7 @@ namespace Tests
 
 
         [TestCase()]
-        public void HandCalculatesType_Straight()
+        public void HandCalculatesType_Straight_without_ace()
         {
             Card[] cards = new Card[5]
             {
@@ -105,6 +105,125 @@ namespace Tests
             var hand = new PokerHand(cards);
 
             Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.Straight));
+        }
+
+        [TestCase()]
+        public void HandCalculatesType_Straight_with_low_ace()
+        {
+            Card[] cards = new Card[5]
+            {
+                new Card(2, Suit.Spades),
+                new Card(3, Suit.Diamonds),
+                new Card(4, Suit.Clubs),
+                new Card(5, Suit.Spades),
+                new Card(14, Suit.Hearts),
+            };
+
+            var hand = new PokerHand(cards);
+
+            Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.Straight));
+        }
+
+        [Test]
+        public void HandCalculatesType_Straight_with_high_ace()
+        {
+            Card[] cards = new Card[5]
+            {
+                new Card(14, Suit.Spades),
+                new Card(13, Suit.Diamonds),
+                new Card(12, Suit.Clubs),
+                new Card(11, Suit.Spades),
+                new Card(10, Suit.Hearts),
+            };
+
+            var hand = new PokerHand(cards);
+
+            Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.Straight));
+        }
+
+        [Test]
+        public void HandCalculatesType_Flush()
+        {
+            Card[] cards = new Card[5]
+            {
+                new Card(5, Suit.Spades),
+                new Card(13, Suit.Spades),
+                new Card(8, Suit.Spades),
+                new Card(11, Suit.Spades),
+                new Card(2, Suit.Spades),
+            };
+
+            var hand = new PokerHand(cards);
+
+            Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.Flush));
+        }
+
+
+        [Test]
+        public void HandCalculatesType_Four_Of_A_Kind()
+        {
+            Card[] cards = new Card[5]
+            {
+                new Card(13, Suit.Spades),
+                new Card(13, Suit.Diamonds),
+                new Card(13, Suit.Hearts),
+                new Card(13, Suit.Clubs),
+                new Card(2, Suit.Spades),
+            };
+
+            var hand = new PokerHand(cards);
+
+            Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.FourOfKind));
+        }
+        [Test]
+        public void HandCalculatesType_Full_House()
+        {
+            Card[] cards = new Card[5]
+            {
+                new Card(13, Suit.Spades),
+                new Card(13, Suit.Diamonds),
+                new Card(13, Suit.Hearts),
+                new Card(2, Suit.Clubs),
+                new Card(2, Suit.Spades),
+            };
+
+            var hand = new PokerHand(cards);
+
+            Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.FullHouse));
+        }
+
+        [Test]
+        public void HandCalculatesType_Straight_Flush()
+        {
+            Card[] cards = new Card[5]
+            {
+                new Card(2, Suit.Spades),
+                new Card(3, Suit.Spades),
+                new Card(4, Suit.Spades),
+                new Card(5, Suit.Spades),
+                new Card(6, Suit.Spades),
+            };
+
+            var hand = new PokerHand(cards);
+
+            Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.StraightFlush));
+        }
+
+        [Test]
+        public void HandCalculatesType_Royal_Flush()
+        {
+            Card[] cards = new Card[5]
+            {
+                new Card(14, Suit.Spades),
+                new Card(13, Suit.Spades),
+                new Card(12, Suit.Spades),
+                new Card(11, Suit.Spades),
+                new Card(10, Suit.Spades),
+            };
+
+            var hand = new PokerHand(cards);
+
+            Assert.That(hand.GetTypeofHand(), Is.EqualTo(TypeOfHand.RoyalFlush));
         }
     }
 }
