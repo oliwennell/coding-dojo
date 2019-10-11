bac38043d95587c5c4c013bdb4f69eefdf6ef5e3
Paul Ayling <paul.ayling@bglgroup.co.uk>
2019-10-11 14:21:08 +0100

    MOB - Herpa Derp

--- arg/src/thing.js
+++ arg/src/thing.js
@@ -1,7 +1,30 @@
 
 function parseArgs(args) {
-    return [
-        { logging: args === "-l" }
-    ]
 
+    const schema = [
+        { 
+            name: 'logging',
+            argumentName: '-l',
+        },
+        { 
+            name: 'ploppy',
+            argumentName: '-p',
+        },
+        // port: {
+        //     name: 'port',
+        //     argumentName: '-p'
+        // }
+    ];
+
+    const resultObjects = [];
+    const splitArgs = args.split(" ");
+    splitArgs.forEach((arg) => {
+        const matchingSchema = schema.find((schemaEntry) => schemaEntry.argumentName === arg)
+
+        const resultObject = {};
+        resultObject[matchingSchema.name] = true;
+        resultObjects.push(resultObject)
+    })
+
+    return resultObjects
 }
