15e2c397383fa107f2fd32e08411ee2244e2cb6b
ellieSlann <eleanor.slann@bglgroup.co.uk>
2020-08-07 16:40:47 +0100

    :sparkles: Adds new run of step and renders new grid

--- python/hello.py
+++ python/hello.py
@@ -1,13 +1,18 @@
 
 def step(previous_step):
+
     return previous_step
+
 
 def render_grid(grid):
     for line in grid:
         for cell in line:
             print(cell, end='')
-        
+
         print("")
 
-grid = [ [ "#", ".", "." ], [ ".", "#", "." ], [ ".", ".", "#" ] ]
+
+grid = [["#", ".", "."], [".", "#", "."], [".", ".", "#"]]
+render_grid(grid)
+grid = step(grid)
 render_grid(grid)
