7d56307e73ff46a01100a2518f6d09dc60da485d
Oli Wennell <oli.wennell@gmail.com>
2020-08-07 15:58:39 +0100

    :construction: Printing things

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,13 @@
-print("Hello, world!")
+
+def step(previous_step):
+    return previous_step
+
+def render_grid(grid):
+    for line in grid:
+        for cell in line:
+            print(cell, end='')
+        
+        print("")
+
+grid = [ [ "#", ".", "." ], [ ".", "#", "." ], [ ".", ".", "#" ] ]
+render_grid(grid)
